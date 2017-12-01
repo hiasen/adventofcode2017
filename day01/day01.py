8d421e46bfdc98dda6bd196880db3ed4c00eb1c9
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2017-12-01 14:27:29 +0100

    Solve part 2 of day 1

--- day01/day01.py
+++ day01/day01.py
@@ -4,6 +4,7 @@ from itertools import chain
 
 s = sys.stdin.read().strip() 
 
+
 def filter_previous(it):
     previous = next(it)
     for x in it:
@@ -11,4 +12,17 @@ def filter_previous(it):
             yield x
         previous = x
 
+
+def halfway_around_pairs(sequence):
+    n = len(sequence)
+    half = n//2
+    assert 2*half == n, "Sequence should be of even length"
+    for x, y in zip(sequence[:half], sequence[half:]):
+        if x == y:
+            yield x
+    
+
+
+
 print(sum(map(int, filter_previous(chain(iter(s), s[0])))))
+print(sum(map(int, halfway_around_pairs(s)))*2)
