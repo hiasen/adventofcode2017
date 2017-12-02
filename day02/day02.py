b159b7acca63c0b98afd5daa36ddb965df454a1f
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2017-12-02 14:58:34 +0100

    Day 2 part 2

--- day02/day02.py
+++ day02/day02.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+from itertools import starmap, combinations
 
 
 def parse_line(line):
@@ -10,5 +11,20 @@ def range_(sequence):
     return max(sequence) - min(sequence)
 
 
-print(sum(map(range_, map(parse_line, sys.stdin))))
+def even_test(x, y):
+    if x < y:
+        x, y = y, x
+    d, r = divmod(x, y)
+    if not r:
+        return d
+    return None
+
+
+def part2(sequence):
+    return next(filter(None, starmap(even_test, combinations(sequence, 2))))
+
+
+table = [parse_line(line) for line in sys.stdin]
+print(sum(map(range_, table)))
+print(sum(map(part2, table))) 
 
