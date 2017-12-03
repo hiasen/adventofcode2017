363b7733179170c715c14a952f0127f66087d05e
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2017-12-03 01:50:25 +0100

    Day 1 part 2 in C

--- day01/day01.c
+++ day01/day01.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int
-main(int argc, char *argv[])
+void
+part1()
 {
 	int first = getchar();
 	int previous = first;
@@ -21,4 +22,38 @@ main(int argc, char *argv[])
 		the_sum += (previous - 48);
 	}
 	printf("%d\n", the_sum);
+}
+
+void
+part2()
+{
+	char buffer[4096];
+	int bytes_read = fread(buffer, 1, 4096, stdin);
+	while (!isdigit(buffer[bytes_read - 1])) {
+		bytes_read--;
+	}
+	if (bytes_read % 2 != 0) {
+		fprintf(stderr, "The number of digits should be even!");
+		exit(1);
+	}
+	int half = bytes_read/2;
+	int the_sum = 0;
+	for (int i = 0; i < half; ++i) {
+		if (buffer[i] == buffer[i+half]) {
+			the_sum += (buffer[i] - 48);
+		}
+	}
+	printf("%d\n", the_sum*2);
+
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc > 1) {
+		part2();
+	} else {
+		part1();
+	}
+	return 0;
 }
