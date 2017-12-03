10e1a5b5af8c30108923c9dbbbac8cbc9b125a8f
Øystein Hiåsen <oystein.hiaasen@gmail.com>
2017-12-03 23:01:17 +0100

    Day 3 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import math
 import sys
+from itertools import count
 
 number = int(sys.argv[1])
 
@@ -12,3 +13,40 @@ else:
 
 print(result)
 
+
+
+border = [1]*8
+while True:
+    next_border = [0] * (len(border)+8)
+
+    sl = len(border)//4
+    nsl = sl + 2
+
+    border[-1] += border[0]
+    border[-2] += border[0]
+
+    for s in range(4):
+        for i in range(sl):
+            j = i + sl * s
+            value = border[j]
+            if value >= number:
+                print(value)
+                exit(0)
+
+            if j+1 < len(border):
+                border[j+1] += value
+
+            next_border[i+nsl*s] += value
+            next_border[i+nsl*s+1] += value
+            next_border[i+nsl*s+2] += value
+
+        if s < 3:
+            border[sl*(s+1)] += border[sl*(s+1) - 2]
+            next_border[nsl*(s+1)] += border[sl*(s+1) - 1]
+            next_border[nsl*(s+1)+1] += border[sl*(s+1) - 1]
+
+    next_border[0] += border[-1]
+    next_border[1] += border[-1]
+
+
+    border = next_border
